63fcbd06ef67c2a9efeb8157455095acd96d2eb2
grapiuna <andrepinto1981@gmail.com>
2020-08-24 09:44:53 -0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$('#assistir').click(function () {
+  window.location = 'https://www.youtube.com/watch?v=SvSkxBYuoQY';
+});
+$('#informacoes').click(function () {
+  window.location = 'https://www.netflix.com/br/title/70178217';
+});
